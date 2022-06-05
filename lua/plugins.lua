e7395dbbacd493db7fcf1b09785ce4d1906cb6c5
EricEricEricJin <jyseric@126.com>
2022-06-05 21:16:26 +0800

    Update.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,8 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'preservim/nerdtree'
+
 Plug 'vim-airline/vim-airline'
 Plug 'neovim/nvim-lspconfig'
 Plug "williamboman/nvim-lsp-installer"
@@ -10,4 +13,9 @@ Plug 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
 Plug 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
 Plug 'L3MON4D3/LuaSnip' -- Snippets plugin
 vim.call('plug#end')
+
+
+-- Configure nerdtree
+vim.cmd([[autocmd VimEnter * NERDTree]])
+vim.keymap.set('n', '<C-b>', function() vim.cmd([[NERDTreeFocus]]) end)
 
