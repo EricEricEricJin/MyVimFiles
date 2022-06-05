703dc646e03e8e3526d802b554d76bb3c6fd309d
EricEricEricJin <jyseric@126.com>
2022-06-05 15:26:46 +0800

    Initialize conf files.

---
+++ lua/luaconfig.lua
@@ -0,0 +1,14 @@
+vim.o.background = 'light'
+vim.bo.filetype = 'lua'
+
+vim.opt.number = true
+
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+
